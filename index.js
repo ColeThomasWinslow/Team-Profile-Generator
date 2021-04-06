20e4c5b862cf5fae6afe402fa1a0094fc54a1319
Cole Winslow <colethomaswinslow7@gmail.com>
2021-04-05 20:32:04 -0700

    clear git and added choices

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-const inquirer = require('inquirer');
+const inquirer = require("inquirer");
+inquirer.prompt([
+  {
+    type: "input",
+    message: "Who is your Manager for this team?",
+    name: "manager",
+  },
+  {
+    type: "input",
+    message: "What is the manager ID?",
+    name: "managerId",
+  },
+  {
+    type: "input",
+    message: "Manager Email:",
+    name: "managerEmail",
+  },
+  {
+    type: "input",
+    message: "Managers office:",
+    name: "managerOffice",
+  },
+  {
+    type: "list",
+    message: "Managers office:",
+    name: "What would you like to update?",
+    choices: ["add a developer", "add a manager", "show teams "],
+  },
+]);
+//   .then((response) =>
+
+//   );
